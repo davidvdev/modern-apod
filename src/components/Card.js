a33c9605d313a7fd1a247140f812f68f03639767
davidvdev <david@davidvdev.com>
2021-11-05 12:11:13 -0500

    fix: cards fade in and stick

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,31 @@ import { useState, useRef, useEffect } from 'react'
 import './Card.sass'
 
 const Card = ({ details }) => {
-    const [isVisible, setIsVisible] = useState(true)
+    const [isVisible, setIsVisible] = useState(false)
     const domRef = useRef()
 
     useEffect(()=>{
+        const options = {
+            rootMargin: '0px',
+            threshold: 0.75
+        }
         const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => setIsVisible(entry.isIntersecting))
-        })
+            entries.forEach(entry => {
+                if (entry.isIntersecting) setIsVisible(true)
+            })
+        },options)
         observer.observe(domRef.current)
-        // return () => observer.unobserve(domRef.current)
+        return () => observer.unobserve(domRef.current)
     },[])
 
 
     return (
         <div
-            ref={domRef} 
+             
             className={`card ${isVisible? 'isvisible': ''}`}
         >
             <h2>{details.title}</h2>
-            <img src={details.url} alt={details.title} />
+            <img src={details.url} alt={details.title} ref={domRef} />
             <p>{details.explanation}</p>
             <div className="card-meta">
                 <span>{details.date}</span>
