c8292ba72b844f48c2599c040d1b178a38cf4de3
davidvdev <david@davidvdev.com>
2021-11-05 15:01:35 -0500

    feature: endless feed functioning

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Card from './components/Card'
 
 function App() {
 
+  const [feedEnd, setFeedEnd] = useState(false)
   const [apiData, setApiData] = useState(null)
-  // const [rerender, setRerender] = useState(0)
   const domRef = useRef()
 
   // NASA Apod API Call
   const apiCall = async () => {
     console.log('API CALL')
     const k = `MdAdCzvrJrRNJyv0elbkdWQw3MtPf2Ll8OdXAMMZ`
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${k}&count=2`
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${k}&count=12`
     const response = await fetch(url)
     const data = await response.json()
-
-    apiData? setApiData([...apiData, ...data]): setApiData(data)
+    console.log(apiData)
+    console.log(data)
+    apiData == null ? setApiData(data): setApiData([...apiData, ...data])
   }
 
-  useEffect(() => { apiCall() }, [])
-  // if( apiData !== null){
-  //   const observer = new IntersectionObserver(entries => {
-  //     entries.forEach(entry => {
-  //         if (entry.isIntersecting){ apiCall()}
-  //     })
-  //   })
-  //   observer.observe(domRef.current)
-  //   return () => observer.unobserve(domRef.current)
-  // }
+  useEffect(() => { apiCall() }, [feedEnd])
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) setFeedEnd(bool => !bool)
+        })
+    })
+    observer.observe(domRef.current)
+    return () => observer.unobserve(domRef.current)
+  },[])  
 
   const randomAdjective = () => {
     const adj = ['amazing', 'cool', 'vast']
@@ -40,15 +42,12 @@ function App() {
     <div className="App">
       <header></header>
       <h1>The Universe is <span className="adj">{randomAdjective()}</span></h1>
-      <div className="gallery" ref={domRef}>
+      <div className="gallery">
       {apiData !== null &&
         apiData.map((item,index) => <Card key={index} details={item}/>)
       }  
       </div>
-      <button onClick={() => {
-        apiCall()
-        // setRerender(rerender + 1)
-        }}>continue</button>
+      <div id={'feed-end'} ref={domRef}/>
     </div>
   );
 }
